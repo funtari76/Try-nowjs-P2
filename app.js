40fef479a103596167f6879fd1a18e4860504208
funtari76 <funtari777@gmail.com>
2022-10-24 04:13:19 +0000

    added to app and questin

---
+++ app.js
@@ -0,0 +1,9 @@
+var questions = [
+    new Question("What is the capital of the USA?", ["Chicago", "Los Angeles", "Washington DC", "London"], "Washington DC"),
+    new Question("What year did World War One begin?", ["1814", "1910", "1935", "1914", ], "1914"),
+    new Question("What is the tallest mountain in the world?", ["Mount Kilimanjaro", "Mount Everest", "", " Mount Lamlam", ], "Mount Everest", ),
+    new Question("How many centimetres in a metre?", ["10", "1000", "100", "10000", ], "100", ),
+    new Question("What company is also the name of one of the longest rivers in the world?", ["Nile", "Amazon", "Parana", "Niger", ], "Amazon", ),
+    new Question("Which language is most spoken in the world", ["Spanish", "Mandarin", "English", "French", ], "English", ),
+    new Question("what language is spoken in Ireland", ["English", "Gaeilge", "Irish", "French", ], "English", )
+];
---
+++ question.js
@@ -0,0 +1,9 @@
+function Question(text, chouices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.correctAnswer = function(choice) {
+    return choice === this.answer;
+}
